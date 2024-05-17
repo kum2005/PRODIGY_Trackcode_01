d115a913c1fad0956005b8b3f76995a514aa986b
kum2005 <sharmakumkum625@gmail.com>
2024-05-17 21:11:01 +0530

    Add files via upload

---
+++ black.js
@@ -0,0 +1,19 @@
+const nav = document.querySelector('.nav');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 200) {
+    nav.classList.add('scrolled');
+  } else {
+    nav.classList.remove('scrolled');
+  }
+});
+
+nav.addEventListener('mouseover', (e) => {
+  if (e.target.tagName === 'A') {
+    nav.classList.add('hovered');
+  }
+});
+
+nav.addEventListener('mouseout', () => {
+  nav.classList.remove('hovered');
+});
